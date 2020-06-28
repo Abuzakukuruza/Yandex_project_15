81e6c570459d0411adbc7dc2b6dcd3cee66e8bbf
Danil Plyusnin <dplusnin0@gmail.com>
2020-06-28 20:19:58 +0600

    First commit

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const { celebrate, Joi } = require('celebrate');
+const { errors } = require('celebrate');
+
+
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const BadGatewayError = require('./errors/bad-gateway-err');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+// подключение логгера запросов
+app.use(requestLogger);
+
+// краш-тест
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new BadGatewayError('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// роуты регистрации и логина
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required().uri(),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), createUser);
+
+// авторизация
+app.use(auth);
+
+// роуты доступа к информации о пользователях и карточках
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
+
+// подключение логгера ошибок
+app.use(errorLogger);
+
+
+// обработчики ошибок
+
+// ошибка при неправильном адресе в строке
+app.use('*', (req, res) => {
+  res.status(404).json({ message: 'Запрашиваемый ресурс не найден' });
+});
+
+// обработчик ошибок celebrate
+app.use(errors());
+
+// централизованный обработчик ошибок
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? err.message : message,
+    });
+});
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
